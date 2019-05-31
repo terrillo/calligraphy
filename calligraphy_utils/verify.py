6326a953a27b5c9d3832d7aa485eb5ae5ed4fbeb
Terrillo Walls <terrillo@terrillo.com>
2019-05-31 16:51:41 -0500

    More verification

--- calligraphy_utils/verify.py
+++ calligraphy_utils/verify.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import sys
 
 required_file_paths = [
     './site.yml'
@@ -7,7 +8,7 @@ required_file_paths = [
 
 required_dir_paths = [
     './posts',
-    './posts/published'
+    './posts/published',
     './themes'
 ]
 
@@ -22,16 +23,44 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def console_error(text):
-    print(bcolors.WARNING + str(text) + bcolors.ENDC)
+    print(bcolors.FAIL + str(text) + bcolors.ENDC)
+
+def verify_site_config():
+    required_fields = [
+        'site_name',
+        'theme'
+    ]
+    site_file = yaml.load(open('./site.yml', 'r'), Loader=yaml.FullLoader)
+    for field in required_fields:
+        if field not in site_file:
+            console_error('site.yml missing "%s"' % field)
+            return False
+
+    return True
+
 
 def verify(log=True):
     for path in required_file_paths:
-        if os.path.isfile('.'+path):
+        if os.path.isfile(path):
+            try:
+                parse = yaml.load(open(path, 'r'), Loader=yaml.FullLoader)
+            except:
+                console_error('"%s" is an invalid yaml file.' % (path))
+                return False
+        else:
             if log:
-                console_error('File "%s" required. View https://github.com/terrillo/calligraphy-cms' % (path))
+                console_error('Required file "%s" not found. View https://github.com/terrillo/calligraphy-cms' % (path))
+            return False
+
+    config_verify = verify_site_config()
+    if not config_verify:
+        return False
 
     for path in required_dir_paths:
-        os.path.isdir('.'+path)
+        if not os.path.isdir(path):
+            if log:
+                console_error('Required directory "%s" not found. View https://github.com/terrillo/calligraphy-cms' % (path))
+            return False
 
     return True
 
