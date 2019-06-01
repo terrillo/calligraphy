dd6856217cf781eec74ac1079a4930f2eae9b065
Terrillo Walls <terrillo@terrillo.com>
2019-05-31 20:34:40 -0500

    starting 0.7.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name='calligraphy',
-        version='0.6.0',
+        version='0.7.0',
         author="Terrillo Walls",
         author_email="terrillo@terrillo.com",
         url="https://github.com/terrillo/calligraphy",
@@ -14,6 +14,11 @@ if __name__ == "__main__":
             'pyyaml',
             'Jinja2>=2.10.1'
         ],
+        project_urls={
+            "Bug Tracker": "https://github.com/terrillo/calligraphy/issues",
+            "Documentation": "https://github.com/terrillo/calligraphy",
+            "Source Code": "https://github.com/terrillo/calligraphy",
+        },
         python_requires=">=3.0.*",
         packages=setuptools.find_packages(),
         classifiers=[
